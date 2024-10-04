e4b42de6ca432a99a83b907f9826120453bb17bf
spcre <spcrey@outlook.com>
2024-10-04 14:54:33 +0800

    opt

--- lib/data.dart
+++ lib/data.dart
@@ -1,3 +1,31 @@
 class User {
+  String name;
+  String phoneNumber;
+  String? email;
 
+  User(this.name, {
+    required this.phoneNumber,
+    this.email
+  });
+}
+
+enum TaskStatus {
+  queueing, inProgress, completed,
+}
+
+enum TaskType {
+  training, evaluation, visualation,
+}
+
+class Task {
+  final String name;
+  final TaskStatus status;
+  final DateTime dateTime;
+  final TaskType type;
+
+  Task(this.name, {
+    required this.status,
+    required this.dateTime,
+    required this.type,
+  });
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,67 +1,85 @@
-import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
+import "package:flutter/material.dart";
+import "package:provider/provider.dart";
+import "data.dart";
 
 void main() {
   runApp(const MyApp());
 }
 
+class AppState extends ChangeNotifier {
+  String? token;
+  User? user;
+  List<Task> tasks = [
+    Task("task1", 
+      status: TaskStatus.queueing, 
+      dateTime: DateTime(2002), 
+      type: TaskType.training, 
+    ),
+    Task("task2", 
+      status: TaskStatus.inProgress, 
+      dateTime: DateTime(2005), 
+      type: TaskType.evaluation, 
+    ),
+    Task("task3", 
+      status: TaskStatus.completed, 
+      dateTime: DateTime(2008), 
+      type: TaskType.visualation, 
+    ),
+  ];
+
+  setToken(String token) {
+    this.token = token;
+    notifyListeners();
+  }
+
+  clearToken() {
+    token = null;
+    notifyListeners();
+  }
+
+  setUser(User user) {
+    this.user = user;
+    notifyListeners();
+  }
+
+  clearUser() {
+    user = null;
+    notifyListeners();
+  }
+}
+
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-
     return ChangeNotifierProvider(
-      create: (context) => TokenState(),
+      create: (context) => AppState(),
       child: MaterialApp(
-        title: 'Ocean Data Super-Resolution Visualization Task Management System',
+        title: "Ocean Data Super-Resolution Visualization Task Management System",
         theme: ThemeData(
           colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
           useMaterial3: true,
         ),
         home: const Scaffold(
-          body: MyHomePage()
+          body: MenuPage(),
         ),
-      )
+      ),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key});
+class MenuPage extends StatefulWidget {
+  const MenuPage({super.key});
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<MenuPage> createState() => _MenuPageState();
 }
 
-class HomePage extends StatelessWidget {
-  const HomePage({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return const Text("Home Page");
-  }
-  
-}
-
-class TaskListPage extends StatelessWidget {
-  const TaskListPage({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return const Text("Task List Page");
-  }
-  
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-
+class _MenuPageState extends State<MenuPage> {
   var selectedIndex = 0;
 
-  @override
-  Widget build(BuildContext context) {
-
+  Widget selectPageByIndex(int selectedIndex) {
     Widget page;
     switch (selectedIndex) {
       case 0:
@@ -74,26 +92,45 @@ class _MyHomePageState extends State<MyHomePage> {
         page = const UserPage();
         break;
       default:
-        throw UnimplementedError('no widget for $selectedIndex');
+        throw UnimplementedError("no widget for $selectedIndex");
     }
+    return page;
+  }
 
+  @override
+  Widget build(BuildContext context) {
     return Row(
       children: [
         SafeArea(
             child: NavigationRail(
+              backgroundColor: const Color.fromARGB(255, 117, 180, 231),
               extended: true,
+              minWidth: 72,
+              minExtendedWidth: 192,
               destinations: const [
                 NavigationRailDestination(
                   icon: Icon(Icons.home),
-                  label: Text('Home'),
+                  label: Text("Home",
+                    style: TextStyle(
+                      color: Colors.white,
+                    ),
+                  ),
                 ),
                 NavigationRailDestination(
                   icon: Icon(Icons.format_list_bulleted),
-                  label: Text('Task List'),
+                  label: Text("Task List",
+                    style: TextStyle(
+                      color: Colors.white,
+                    ),
+                  ),
                 ),
                 NavigationRailDestination(
                   icon: Icon(Icons.person),
-                  label: Text('user'),
+                  label: Text("User",
+                    style: TextStyle(
+                      color: Colors.white,
+                    ),
+                  ),
                 ),
               ],
               selectedIndex: selectedIndex,
@@ -106,11 +143,168 @@ class _MyHomePageState extends State<MyHomePage> {
           ),
           Expanded(child: Container(
               color: Theme.of(context).colorScheme.primaryContainer,
-              child: page,
+              child: selectPageByIndex(selectedIndex),
             ),
           )
       ],
     );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Home Page"),
+      ),
+      body: const Placeholder(),
+    );
+  }
+}
+
+class TaskListPage extends StatelessWidget {
+  const TaskListPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Task List Page"),
+      ),
+      body: Column(
+        children: [
+          TextButton(
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => const NewTaskPage()),
+              );
+            }, 
+            child: const Text("add"),
+          ),
+          const ListPage(),
+        ],
+      ),
+    );
+  }
+}
+
+class ListPage extends StatelessWidget {
+  const ListPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    var appState = context.watch<AppState>();
+    return SizedBox(
+      height: 320,
+      child: ListView(
+      children: [
+        for (var task in appState.tasks)
+          ListTile(
+            title: TaskItem(task: task),
+          )
+      ],
+    ),
+    );
+  }
+}
+
+class TaskItem extends StatelessWidget {
+  const TaskItem({
+    super.key,
+    required this.task,
+  });
+
+  final Task task;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Text(task.name),
+        TextButton(
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => TaskDetailPage(
+                  task: task,
+                )
+              ),
+            );
+        }, 
+          child: const Text("detail")
+        ),
+      ],
+    );
+  }
+}
+
+class TaskDetailPage extends StatelessWidget {
+  final Task task;
+
+  const TaskDetailPage({super.key, required this.task});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(task.name),
+      ),
+      body: TextButton(
+        onPressed: () {
+          Navigator.pop(context);
+        }, 
+        child: const Text("back")
+      ),
+    ); 
+  }
+  
+}
+
+class NewTaskPage extends StatefulWidget {
+  const NewTaskPage({super.key});
+
+  @override
+  State<NewTaskPage> createState() => _NewTaskPageState();
+}
+
+class _NewTaskPageState extends State<NewTaskPage> {
+  Future<bool> submit() async {
+    await Future.delayed(const Duration(seconds: 1));
+    return true;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("New Task Page"),
+      ),
+      body: Column(
+        children: [
+          TextButton(
+            onPressed: () async {
+              if (await submit()) {
+                setState(() {
+                  Navigator.pop(context);
+                });
+              }
+            }, 
+            child: const Text("submit")
+          ),
+          TextButton(
+            onPressed: () {
+              Navigator.pop(context);
+            }, 
+            child: const Text("cancel")
+          ),
+        ],
+      ),
+    ); 
   }
 }
 
@@ -125,113 +319,192 @@ class _UserPageState extends State<UserPage> {
 
   @override
   Widget build(BuildContext context) {
-
-    var tokenState = context.watch<TokenState>();
-
-    if (tokenState.token == null) {
-      return Column(
-        children: [
-          const Text("Not Login"),
-          UserLoginPage(setTokenCallback: () { 
-            setState(() {
-                tokenState.token = "user";
-              });
-          },),
-        ],
-      );
+    var appState = context.watch<AppState>();
+    if (appState.token == null) {
+      return const UserLoginPage();
     } else {
-      return Column(
-        children: [
-          const Text("In Login"),
-          TextButton(
-            onPressed: () {
-              setState(() {
-                tokenState.token = null;
-              });
-            }, 
-            child: const Text("Exit Login")
-          )
-        ],
-      );
+      return const UserInfoPage();
     }
   }
 }
 
-class UserLoginPage extends StatefulWidget {
-  const UserLoginPage({super.key, required this.setTokenCallback});
+class UserLoginPage extends StatelessWidget {
+  const UserLoginPage({super.key});
 
-  final VoidCallback setTokenCallback;
-
-  @override
-  State<UserLoginPage> createState() => _UserLoginPageState();
-  
-  void setToken() {
-    setTokenCallback();
-  }
-}
-
-class _UserLoginPageState extends State<UserLoginPage> {
-  
   @override
   Widget build(BuildContext context) {
-    return UserLoginForm(setTokenCallBack: () {
-      widget.setToken();
-    },);
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("User Login Page"),
+      ),
+      body: const UserLoginForm(),
+    );
   }
 }
 
 class UserLoginForm extends StatefulWidget {
-  const UserLoginForm({super.key, required this.setTokenCallBack});
-
-  final VoidCallback setTokenCallBack;
+  const UserLoginForm({super.key});
 
   @override
   State<UserLoginForm> createState() => _UserLoginFormState();
-  
-  void setToken() {
-    setTokenCallBack();
-  }
-}
-
-class TokenState extends ChangeNotifier {
-  String? token;
 }
 
 class _UserLoginFormState extends State<UserLoginForm> {
-
   final _usernameController = TextEditingController();
   final _passwordController = TextEditingController();
 
-  Future<bool> fetchData(String username, String password) async {
+  Future<User?> login(String username, String password) async {
     await Future.delayed(const Duration(seconds: 1));
-    if (password == "crey") {
-      return true;
+    if (username == "spcrey" && password == "crey199854") {
+      return User("Crey",
+        phoneNumber: "18243280995"
+      );
     } else {
-      return false;
+      return null;
     }
   }
 
   @override
   Widget build(BuildContext context) {
-
+    var appState = context.watch<AppState>();
     return Column(
       children: [
-        TextFormField(
-          controller: _usernameController,
+        Row(
+          children: [
+            const Card(
+              child: Text("username"),
+            ),
+            const SizedBox(width: 10),
+            SizedBox(
+              width: 240,
+              child: TextFormField(
+                controller: _usernameController,
+              ),
+            )
+          ],
         ),
-        TextFormField(
-          controller: _passwordController,
+        Row(
+          children: [
+            const Text("password"),
+            const SizedBox(width: 10),
+            SizedBox(
+              width: 240,
+              child: TextFormField(
+                controller: _passwordController,
+              ),
+            )
+          ],
         ),
         TextButton(
           onPressed: () async {
-            bool status = await fetchData(_usernameController.text, _passwordController.text);
-            if (status) {
-              widget.setToken();
+            User? user = await login(_usernameController.text, _passwordController.text);
+            if (user != null) {
+              appState.setToken("token");
+              appState.setUser(user);
             }
           }, 
           child: const Text("login")
         ),
-      ],
+        TextButton(
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => const UserRegisterPage()),
+            );
+          }, 
+          child: const Text("register"),
+        ),
+      ]
+    );
+  }
+}
+
+class UserInfoPage extends StatelessWidget {
+  const UserInfoPage({super.key});
+
+  Future<bool> logout() async {
+    await Future.delayed(const Duration(seconds: 1));
+    return true;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    var appState = context.watch<AppState>();
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("User Info Page"),
+      ),
+      body: Column(
+        children: [
+          const Text("user"),
+          TextButton(
+            onPressed: () async {
+              if (await logout()) {
+                appState.clearToken();
+                appState.clearUser();
+              }
+            }, 
+            child: const Text("logout"),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class UserRegisterPage extends StatefulWidget {
+  const UserRegisterPage({super.key});
+
+  @override
+  State<UserRegisterPage> createState() => _UserRegisterPageState();
+}
+
+class _UserRegisterPageState extends State<UserRegisterPage> {
+  final _usernameController = TextEditingController();
+  final _firstPasswordController = TextEditingController();
+  final _secondPasswordController = TextEditingController();
+
+  Future<bool> register(String username, String firstPassword, String secondPassword) async {
+    await Future.delayed(const Duration(seconds: 1));
+    return true;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("User Register Page"),
+      ),
+      body: Column(
+        children: [
+          TextFormField(
+            controller: _usernameController,
+          ),
+          TextFormField(
+            controller: _firstPasswordController,
+          ),
+          TextFormField(
+            controller: _secondPasswordController,
+          ),
+          TextButton(
+            onPressed: () async {
+              if (await register(_usernameController.text, _firstPasswordController.text, _secondPasswordController.text)) {
+                setState(() {
+                  Navigator.pop(context);
+                });
+              }
+            }, 
+            child: const Text("register")
+          ),
+          TextButton(
+            onPressed: () {
+              Navigator.pop(context);
+            }, 
+            child: const Text("back")
+          ),
+        ],
+      ),
     );
   }
 }
